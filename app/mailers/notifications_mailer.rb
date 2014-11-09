57c5140827348ac2fdb92f7858376880a382190a
genevievenielsen <genevieve.b.nielsen@gmail.com>
2014-11-09 12:14:17 -0600

    Updated the mailer

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -10,12 +10,19 @@ class NotificationsMailer < ActionMailer::Base
     :subject => "Feedback from #{contact_first_name} #{contact_last_name}",
     :text => "How did you hear about us - #{advertising_options}.  Request related to - #{request }.  Feedback - #{feedback}"
 
-    RestClient.post "https://api:"+ ENV['mailgun_key'] +
-    "@api.mailgun.net/v2/mrelief.com/messages",
-    :from => "#{contact_email}",
-    :to => ENV['rose_email'],
-    :subject => "Feedback from #{contact_first_name} #{contact_last_name}",
-    :text => "How did you hear about us - #{advertising_options}.  Request related to - #{request }.  Feedback - #{feedback}"
+
+  end
+
+  def send_simple_message_to_rose(contact_first_name, contact_last_name, contact_email,
+        advertising_options, request, feedback)
+      RestClient.post "https://api:"+ ENV['mailgun_key'] +
+       "@api.mailgun.net/v2/mrelief.com/messages",
+       :from => "#{contact_email}",
+       :to => ENV['rose_email'],
+       :subject => "Feedback from #{contact_first_name} #{contact_last_name}",
+       :text => "How did you hear about us - #{advertising_options}.  Request related to - #{request }.  Feedback - #{feedback}"
 
   end
 end
+
+
