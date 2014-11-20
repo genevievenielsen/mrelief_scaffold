c155ee9cc63bacf5d7dba2817a7fa1db0b54db31
genevievenielsen <genevieve.b.nielsen@gmail.com>
2014-11-20 11:25:40 -0600

    Updated Twilio code

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -126,7 +126,7 @@ class TwilioController < ApplicationController
    end
 
 
-   if session["page"] == "snap_income_question" && session["counter"] == 8 || session["counter"] == 9
+   if session["page"] == "snap_income_question" && session["counter"] == 8 || session["page"] == "snap_income_question" && session["counter"] == 9
      session["income"] = params[:Body].strip
 
      if session["income"] !~ /\D/
@@ -158,14 +158,7 @@ class TwilioController < ApplicationController
         snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => snap_dependent_no })
       end
 
-      user_zipcode = session["zipcode"]
-      @zipcode = user_zipcode << ".0"
-      @lafcenter = LafCenter.find_by(:zipcode => @zipcode)
 
-      if @lafcenter.present?
-      else
-        @lafcenter = LafCenter.find_by(:id => 10)
-      end
 
       if snap_gross_income < snap_eligibility.snap_gross_income
         message = "You may be in luck! You likely qualify for foodstamps. To access your food stamps, go to #{@lafcenter.center} at #{@lafcenter.address} #{@lafcenter.city}, #{@lafcenter.zipcode.to_i }, #{@lafcenter.telephone}.  To check other programs, type 'menu'."
@@ -173,54 +166,7 @@ class TwilioController < ApplicationController
         message = "Based on your household size and income, you likely do not qualify for food stamps. Go to Direct2Food at http://www.direct2food.org to locate the food pantries, soup kitchens and meal programs near you. To check other programs, type 'menu'."
       end
    end
-
-   if session["page"] == "snap_income_question" && session["counter"] == 9
-     session["income"] = params[:Body].strip
-
-     if session["income"] !~ /\D/
-       session["income"] = session["income"].to_i
-     else
-       if session["income"].include?("dollars")
-         session["income"].slice!"dollars"
-       end
-       if session["income"].include?("$")
-         session["income"].slice!"$"
-       end
-       if session["income"].include?(",")
-         session["income"].slice!","
-       end
-       session["income"] = session["income"].in_numbers
-     end
-
-     age = session["age"].to_i
-     snap_dependent_no = session["dependents"].to_i
-     snap_gross_income = session["income"].to_i
-
-      if age <= 59
-        snap_eligibility = SnapEligibility.find_by({ :snap_dependent_no => snap_dependent_no })
-      else
-        snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => snap_dependent_no })
-      end
-
-      if @disability.present?
-        snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => snap_dependent_no })
-      end
-
-      user_zipcode = session["zipcode"]
-      @zipcode = user_zipcode << ".0"
-      @lafcenter = LafCenter.find_by(:zipcode => @zipcode)
-
-      if @lafcenter.present?
-      else
-        @lafcenter = LafCenter.find_by(:id => 10)
-      end
-
-      if snap_gross_income < snap_eligibility.snap_gross_income
-        message = "You may be in luck! You likely qualify for foodstamps. To access your food stamps, go to #{@lafcenter.center} at #{@lafcenter.address} #{@lafcenter.city}, #{@lafcenter.zipcode.to_i }, #{@lafcenter.telephone}.  To check other programs, type 'menu'."
-      else
-        message = "Based on your household size and income, you likely do not qualify for food stamps. Go to Direct2Food at http://www.direct2food.org to locate the food pantries, soup kitchens and meal programs near you. To check other programs, type 'menu'."
-      end
-   end
+  end
 
    # Food stamps user is in school
    if session["page"] == "snap_zipcode_question" && session["counter"] == 3
