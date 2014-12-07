d5272c09702495c4c1677295753221ee226f89ad
Rene M. Paccha <paccha@gmail.com>
2014-12-07 00:45:33 -0600

    3 initial iso 2 language codes

--- app/controllers/set_language_controller.rb
+++ app/controllers/set_language_controller.rb
@@ -1,10 +1,13 @@
 class SetLanguageController < ApplicationController
   def english
+    R18n.set('en')
   end
 
   def spanish
+    R18n.set('es')
   end
 
   def polish
+    R18n.set('pl')
   end
 end
