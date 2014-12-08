14d0de119f2f0e5748c643d8a2900d94950d71d8
genevievenielsen <genevieve.b.nielsen@gmail.com>
2014-12-08 10:39:25 -0600

    Updated ivr code again

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,12 +6,17 @@ class TwilioController < ApplicationController
   #    end
   #    render xml: twiml.to_xml
   #  end
+  include Webhookable
+
+  after_filter :set_header
+
+  skip_before_action :verify_authenticity_token
 
   def voice
       response = Twilio::TwiML::Response.new do |r|
            r.Play 'https://dl.dropboxusercontent.com/s/vaq6et51o8ohwxz/mRelief.mp3?dl=0'
-      end.text
-      # render_twiml response
+      end
+      render text: response.text
     end
 
   require 'numbers_in_words'
