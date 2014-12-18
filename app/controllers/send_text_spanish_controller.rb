19148e3cf9addfe63867471123b2892ab38e9c4f
genevievenielsen <genevieve.b.nielsen@gmail.com>
2014-12-18 15:56:08 -0600

    Set up the spanish twilio controller

---
+++ app/controllers/send_text_spanish_controller.rb
@@ -0,0 +1,16 @@
+class SendTextSpanishController < ApplicationController
+
+  def send_text_message
+
+    account_sid = ENV['TWILIO_SPANISH_SID']
+    auth_token = ENV['TWILIO_SPANISH_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
+        :to => "+18477672332",     # Replace with your phone number
+        :from => ENV['TWILIO_SPANISH_PHONE_NUMBER']   # Replace with your Twilio number
+        )
+
+  end
+
+end
