abc396c48e2993878fa2f7342fa6f80bc029541c
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-01-15 20:36:48 -0600

    Updated send spanish code

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -18,7 +18,7 @@ class SendTextController < ApplicationController
 
     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
         :to => "+18477672332",     # Replace with your phone number
-        :from => ENV['TWILIO_PHONE_NUMBER']   # Replace with your Twilio number
+        :from => ENV['TWILIO_SPANISH_NUMBER']   # Replace with your Twilio number
         )
   end
 
