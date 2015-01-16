62b419aef796a5598c8ceadfacfc829ed51a9dbd
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-01-16 14:31:27 -0600

    Updated the Twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -27,6 +27,7 @@ class TwilioController < ApplicationController
   end
    if session["counter"] == 0
     message = "Welcome to mRelief! We help you check your eligibility for benefits. For foodstamps, text 'food'. For RTA ride free, text 'ride.' For Medicaid, text 'medicaid.' For Medicare Cost Sharing, text 'medicare.' If you make a mistake, send the message 'reset'."
+    session["counter"] = 0
    end
 
    if params[:Body].strip.downcase == "menu"
