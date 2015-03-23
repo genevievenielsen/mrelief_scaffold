490f1ccecc748045d1675ed359703fb5c63d6901
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-03-23 11:15:48 -0500

    Set up global screening questions for Food Stamps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,22 @@ class PagesController < ApplicationController
   end
 
   def filtered_programs
+    # CHICAGO PROGRAMS
     @programs = []
+
+    # food stamps logic
+    if  params[:disabled] != "No"
+       @snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => params[:dependent_no].to_i})
+    elsif params[:age].to_i <= 59
+      @snap_eligibility = SnapEligibility.find_by({ :snap_dependent_no => params[:dependent_no].to_i })
+    elsif params[:age].to_i > 59
+      @snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => params[:dependent_no].to_i})
+    end
+
+    if params[:gross_income].to_i < @snap_eligibility.snap_gross_income
+      @snap = Program.find_by(:name_en => "Food Stamps")
+      @programs.push(@snap)
+    end
 
     # if the user is older than 64 OR disabled, show the rta ride free program
     if params[:age].to_i > 64 || params[:disabled] != "No"
@@ -19,9 +34,21 @@ class PagesController < ApplicationController
 
     # medicare cost sharing - householdsize and gross monthly income
 
-    # programs that we can't screen for with global questions - rental assistance,
+    # programs that we can't screen for with global questions - rental assistance (90 day gross income),
+    #aabd cash assistance, tanf cash assistance
 
-     # @programs = Program.all
+
+    # ILLINOIS PROGRAMS
+
+    # child care vouchers - householdsize and gross monthly income
+
+    # wic - household size and gross monthly income
+
+    # early head start - household size and gross monthly income
+
+    # head start - household size
+
+
   end
 
   def about_us
