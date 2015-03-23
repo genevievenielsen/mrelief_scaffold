8065af58c20580ab283e9ae58dea67d714999d53
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-03-23 11:36:29 -0500

    Set up global screening questions for Medicaid and Medicare Cost Sharing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     # CHICAGO PROGRAMS
     @programs = []
 
-    # food stamps logic
+    # food stamps logic, based on household size, age, disability status and gross monthly income
     if  params[:disabled] != "No"
        @snap_eligibility = SnapEligibilitySenior.find_by({ :snap_dependent_no => params[:dependent_no].to_i})
     elsif params[:age].to_i <= 59
@@ -22,17 +22,29 @@ class PagesController < ApplicationController
       @programs.push(@snap)
     end
 
-    # if the user is older than 64 OR disabled, show the rta ride free program
+    # rta ride free logic, based on age and disability status
     if params[:age].to_i > 64 || params[:disabled] != "No"
       @rta_ride_free = Program.find_by(:name_en => "RTA Ride Free")
       @programs.push(@rta_ride_free)
     end
 
-    # all kids - householdsize and gross monthly income
+    # all kids - householdsize and gross monthly income - slighlty more complicated because we ask how many children
+
 
     # medicaid - householdsize and gross monthly income
+    @medicaid_eligibility = Medicaid.find_by({ :medicaid_household_size => params[:dependent_no].to_i})
+    if params[:gross_income].to_i < @medicaid_eligibility.medicaid_gross_income
+      @medicaid = Program.find_by(:name_en => "Medicaid")
+      @programs.push(@medicaid)
+    end
 
     # medicare cost sharing - householdsize and gross monthly income
+    @medicare_sharing_eligibility = MedicareCostSharing.find_by({ :household_size => params[:dependent_no].to_i })
+    if params[:gross_income].to_i < @medicare_sharing_eligibility.premium_only
+      @medicare_cost_sharing = Program.find_by(:name_en => "Medicare Cost Sharing")
+      @programs.push(@medicare_cost_sharing)
+    end
+
 
     # programs that we can't screen for with global questions - rental assistance (90 day gross income),
     #aabd cash assistance, tanf cash assistance
