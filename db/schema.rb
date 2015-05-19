777cc6dec6279de07315c9b29c6dfbf4201babb0
Marina Goldshteyn <marina.goldshteyn@gmail.com>
2015-05-19 18:54:56 -0500

    Adding new styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150410144330) do
+ActiveRecord::Schema.define(version: 20150427212101) do
 
   create_table "aabd_cash_data", force: true do |t|
     t.string   "household_size"
