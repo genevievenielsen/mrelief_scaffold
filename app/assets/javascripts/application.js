c0aad34adcca4773d7f9799741521e090b2a607b
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-05-22 09:55:55 -0500

    Cleaned up JavaScript for popovers and calculator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,40 @@
 
 $(document).ready(function() {
 
-  $(function ()
-    { $("[data-toggle='popover']").popover();
+  //popover code
+  $(function (){
+    $("[data-toggle='popover']").popover();
+  });
+
+  $('body').on('click', function (e) {
+      $('[data-toggle="popover"]').each(function () {
+          //the 'is' for buttons that trigger popups
+          //the 'has' for icons within a button that triggers a popup
+          if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+              $(this).popover('hide');
+          }
+      });
+  });
+
+  //Scrolling calculator
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 310 && $(this).width() > 750) {
+      $('.calculator_box').addClass('sticky_calculator');
+    } else {
+      $('.calculator_box').removeClass('sticky_calculator');
+    }
+  });
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() < 310) {
+      $('.calculator_box').removeClass('sticky_calculator');
+    }
+  });
+
+  $(window).scroll(function() {
+    if($(this).width() < 750) {
+      $('.calculator').removeClass('sticky_calculator');
+    }
   });
 
 
