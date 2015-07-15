bfffd822b06a6d09b28b70e0e79c8d220ea08f01
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-07-15 13:57:28 -0500

    Commented out twilio code

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -1,25 +1,25 @@
-class SendTextController < ApplicationController
+# class SendTextController < ApplicationController
 
-  def send_text_message
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
+#   def send_text_message
+#     account_sid = ENV['TWILIO_SID']
+#     auth_token = ENV['TWILIO_TOKEN']
+#     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-        :to => "+18477672332",     # Replace with your phone number
-        :from => ENV['TWILIO_PHONE_NUMBER']   # Replace with your Twilio number
-        )
-  end
+#     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
+#         :to => "+18477672332",     # Replace with your phone number
+#         :from => ENV['TWILIO_PHONE_NUMBER']   # Replace with your Twilio number
+#         )
+#   end
 
-  def send_spanish_text_message
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
+#   def send_spanish_text_message
+#     account_sid = ENV['TWILIO_SID']
+#     auth_token = ENV['TWILIO_TOKEN']
+#     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-        :to => "+18477672332",     # Replace with your phone number
-        :from => ENV['TWILIO_SPANISH_NUMBER']   # Replace with your Twilio number
-        )
-  end
+#     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
+#         :to => "+18477672332",     # Replace with your phone number
+#         :from => ENV['TWILIO_SPANISH_NUMBER']   # Replace with your Twilio number
+#         )
+#   end
 
-end
+# end
