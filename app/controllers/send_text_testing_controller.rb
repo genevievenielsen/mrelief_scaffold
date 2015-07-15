bfc13a2090b1ad67252085f749f18e51bc22a3df
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-07-15 14:01:25 -0500

    Commented out twilio code

--- app/controllers/send_text_testing_controller.rb
+++ app/controllers/send_text_testing_controller.rb
@@ -1,16 +1,16 @@
-lass SendTextTestingController < ApplicationController
+# class SendTextTestingController < ApplicationController
 
-  def send_spanish_text_message
+#   def send_spanish_text_message
 
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
+#     account_sid = ENV['TWILIO_SID']
+#     auth_token = ENV['TWILIO_TOKEN']
+#     @client = Twilio::REST::Client.new account_sid, auth_token
 
-    message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-        :to => ENV['RECEIPENT_NUMBER'],     # Replace with your phone number
-        :from => ENV['TWILIO_TESTING_PHONE_NUMBER']   # Replace with your Twilio number
-        )
+#     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
+#         :to => ENV['RECEIPENT_NUMBER'],     # Replace with your phone number
+#         :from => ENV['TWILIO_TESTING_PHONE_NUMBER']   # Replace with your Twilio number
+#         )
 
-  end
+#   end
 
-end
+# end
