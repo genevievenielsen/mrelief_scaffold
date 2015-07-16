72badad6184482ef7da31d22a9c5c1b78359b603
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-07-16 18:36:14 -0500

    Set up early learning screener on Twilio

--- app/controllers/send_text_controller.rb
+++ app/controllers/send_text_controller.rb
@@ -6,7 +6,7 @@ class SendTextController < ApplicationController
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-        :to => "+18477672332",     # Replace with your phone number
+        :to => ENV['RECEIPENT_NUMBER'],     # Replace with your phone number
         :from => ENV['TWILIO_PHONE_NUMBER']   # Replace with your Twilio number
         )
   end
@@ -17,9 +17,22 @@ class SendTextController < ApplicationController
     @client = Twilio::REST::Client.new account_sid, auth_token
 
     message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
-        :to => "+18477672332",     # Replace with your phone number
+        :to => ENV['RECEIPENT_NUMBER'],     # Replace with your phone number
         :from => ENV['TWILIO_SPANISH_NUMBER']   # Replace with your Twilio number
         )
+  end
+
+  def send_test_text_message
+
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+
+    message = @client.account.messages.create(:body => "Jenny please?! I love you <3",
+        :to => ENV['RECEIPENT_NUMBER'],     # Replace with your phone number
+        :from => ENV['TWILIO_TESTING_NUMBER']   # Replace with your Twilio number
+        )
+
   end
 
 end
