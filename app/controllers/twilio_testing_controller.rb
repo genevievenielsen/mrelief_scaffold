9f8355822e67a7908f6dc69b37ed3fe97244173b
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-07-16 23:09:10 -0500

    Update again

--- app/controllers/twilio_testing_controller.rb
+++ app/controllers/twilio_testing_controller.rb
@@ -182,7 +182,7 @@ class TwilioTestingController < ApplicationController
       end
       @user.gross_monthly_income = income
       # Determine income eligible programs
-      puts "This is the #{@user.hou}"
+      puts "This is the #{@user.household_size}"
       income_row = EarlyLearningIncomeCutoff.find_by(household_size: @user.household_size.to_i)
       puts "This is the income row: #{income_row}"
 
