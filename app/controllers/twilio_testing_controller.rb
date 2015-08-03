1732f7de1a78f0f9be058c43a721562531c088e9
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-08-03 10:15:13 -0500

    Update twilio test code for early learning screener

--- app/controllers/twilio_testing_controller.rb
+++ app/controllers/twilio_testing_controller.rb
@@ -207,7 +207,7 @@ class TwilioTestingController < ApplicationController
       end
       @user.income_type = @user_income_type.try(:to_s)
 
-      if @user.income_type == ['Greater than Type 2'] && @user.three_and_under == true
+      if @user_income_type == ['Greater than Type 2'] && @user.three_and_under == true
         message = "Based on your household size and income you do not qualify for early learning programs, but please call Illinois Action For Children Community Referral Team at 312-823-1100 for more information."
         @user.early_learning_eligible = false
         @user.ccap_eligible = false
